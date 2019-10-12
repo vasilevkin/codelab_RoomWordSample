54478addc43125c45c63491f53c21483a2ac03c6
Sergey Vasilevkin <svasilev@mera.ru>
2019-10-12 12:49:07 +0300

    Create the DAO.

---
+++ app/src/main/java/com/vasilevkin/roomwordsample/WordDao.kt
@@ -0,0 +1,19 @@
+package com.vasilevkin.roomwordsample
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+
+@Dao
+interface WordDao {
+
+    @Query("SELECT * from word_table ORDER BY word ASC")
+    fun getAllWords(): List<Word>
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insert(word: Word)
+
+    @Query("DELETE FROM word_table")
+    suspend fun deleteAll()
+}
