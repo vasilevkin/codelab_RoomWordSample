4e66d85168b4c2769661f347a359c2b3a3a98f79
Sergey Vasilevkin <svasilev@mera.ru>
2019-10-12 12:54:33 +0300

    LiveData as a return type .

--- app/src/main/java/com/vasilevkin/roomwordsample/WordDao.kt
+++ app/src/main/java/com/vasilevkin/roomwordsample/WordDao.kt
@@ -1,5 +1,6 @@
 package com.vasilevkin.roomwordsample
 
+import androidx.lifecycle.LiveData
 import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
@@ -7,9 +8,9 @@ import androidx.room.Query
 
 @Dao
 interface WordDao {
-
+    
     @Query("SELECT * from word_table ORDER BY word ASC")
-    fun getAllWords(): List<Word>
+    fun getAllWords(): LiveData<List<Word>>
 
     @Insert(onConflict = OnConflictStrategy.IGNORE)
     suspend fun insert(word: Word)
